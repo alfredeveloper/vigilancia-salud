b14c90541513c9726734df05431b7795ac1cedc0
Alfred <alfredeveloper@gmail.com>
2020-11-19 09:14:40 -0500

    change port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 5000,
+    port: process.env.PORT || 4000,
     db: process.env.MONGODB_URI || 'mongodb://localhost:27017/vigilanciadb',
     SECRET_TOKEN: 'miclavedetokens'
 }
