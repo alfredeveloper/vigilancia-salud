05107cb72f9d187a6f642974a6c41e8a99cefbe0
Alfred <alfredeveloper@gmail.com>
2021-04-19 18:56:51 -0500

    tabla en horizontal

--- controllers/follow_up.js
+++ controllers/follow_up.js
@@ -209,10 +209,165 @@ function updateFollowUp(req, res) {
 }
 
 function getFollowsByPatient(req, res) {
-    ClinicalFollowUp.find({patient: req.params.id}, (err, patients) => {
+    Patient.findById(req.params.id, (err, patient) => {
         if(err) return res.status(500).send({message: `Error en el servidor ${err}`, status: false});
 
-        return res.status(201).send({message: `Seguimientos encontrados`, status: true, data: patients});
+        ClinicalFollowUp.find({patient: req.params.id}, (err, follows) => {
+            if(err) return res.status(500).send({message: `Error en el servidor ${err}`, status: false});
+            
+            let data2 = [{0: 'PA'}, {0: 'FC'}, {0: 'FR'}, {0: 'TEMPERATURA'}, {0: 'PULSIOXIMETRIA'},{0: 'TOS'}, {0: 'DOLOR DE GARGANTA'}, {0: 'CONGESTION NASAL'}, {0: 'FIEBRE'}, {0: 'MALESTAR GENERAL'},{0: 'DIFICULTAD RESPIRATORIA'}, {0: 'DIARREA'}, {0: 'NAUSEA / VOMITO'}, {0: 'CEFALEA'}, {0: 'IRRITABILIDAD / CONFUSION'},{0: 'DOLOR MUSCULAR'}, {0: 'DOLOR ABDOMINAL'}, {0: 'DOLOR DE PECHO'}, {0: 'DOLOR EN ARTICULACIONES'}, {0: 'OTRO'},{0: 'DISNEA'}, {0: 'TAQUIPNEA (>= 22 PM)'}, {0: 'SATURACIÓN DE OXIGENO < 92%'}, {0: 'ALTERACIÓN DE LA CONCIENCIA'}, {0: 'FOSFATO DE CLORIQUINA'},{0: 'HIDORXICLOROQUINA'}, {0: 'HIDORXICLOROQUINA + AZITROMICINA'}, {0: 'OTRO'}, {0: 'ANTIBIÓTICO (especificar)'}, {0: 'ANTIVIRAL (especificar)'},{0: 'ENOXAPARIMA mg'}, {0: 'OTRO (especificar)'}, {0: 'ESPECIFICAR MEDICAMENTO'}, {0: 'ESTADO'}];
+            let headers = ['Indicadores'];
+
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                headers.push(element.created_at)
+            }
+
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[0][`${index+1}`] = element['pa'];
+            }
+
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[1][index+1] = element['fc'];
+            }
+
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[2][index+1] = element['fr'];
+            }
+
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[3][index+1] = element['temperatura'];
+            }
+
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[4][index+1] = element['pulsioximetria'];
+            }
+
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[5][index+1] = element['tos'];
+            }
+            
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[6][index+1] = element['dolor_garganta'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[7][index+1] = element['congestion_nasal'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[8][index+1] = element['fiebre'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[9][index+1] = element['malestar_general'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[10][index+1] = element['dificultad_respiratoria'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[11][index+1] = element['diarrea'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[12][index+1] = element['nausea_vomito'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[13][index+1] = element['cefalea'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[14][index+1] = element['irritabilidad_confusion'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[15][index+1] = element['dolor_muscular'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[16][index+1] = element['dolor_abdominal'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[17][index+1] = element['dolor_pecho'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[18][index+1] = element['dolor_articulaciones'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[19][index+1] = element['otro_signo_sintoma'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[20][index+1] = element['disnea'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[21][index+1] = element['taquipnea'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[22][index+1] = element['saturacion_oxigeno'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[23][index+1] = element['alteracion_conciencia'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[24][index+1] = element['fosfato_cloroquina'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[25][index+1] = element['hidorxicloroquina'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[26][index+1] = element['azitromicina'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[27][index+1] = element['otro_tratamiento_especifico'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[28][index+1] = element['antibiotico'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[29][index+1] = element['antiviral'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[30][index+1] = element['onoxapirina'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[31][index+1] = element['otro_anticoagulatorio'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[32][index+1] = element['especificar_medicamento'];
+            }
+            for (let index = 0; index < follows.length; index++) {
+                const element = follows[index];
+                data2[33][index+1] = element['estado_evolucion'];
+            }
+
+            return res.status(201).send({message: `Seguimientos encontrados`, status: true, data: follows, data2, headers, patient});
+        })
     })
 }
 
